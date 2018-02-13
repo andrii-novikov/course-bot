23b4e738d76082f647d004b086d85a509795c1a4
Andrii Novikov <andruha2921@gmail.com>
2018-02-13 13:34:34 +0200

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,7 @@ set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/sites/#{fetch(:application)}"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :rack_pid,  "#{release_path}/tmp/rack.pid"
 set :bundle_binstubs, nil
 
 set :linked_files, %w{ config/application.yml }
@@ -23,11 +24,19 @@ namespace :deploy do
       upload!("config/application.yml", "#{deploy_to}/shared/config/application.yml")
     end
   end
+end
 
+namespase :app do
   desc 'Restart application'
-  task :restart do
+  task :start do
     on roles(:app), in: :sequence, wait: 5 do
+      execute "rackup -D -P #{fetch(:rack_pid)}"
+    end
+  end
 
+  task :stop do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute "rackup -D -P #{fetch(:rack_pid)}"
     end
   end
 end
